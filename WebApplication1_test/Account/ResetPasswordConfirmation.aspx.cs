b9d59d3b48febafa30a86bee75c7bfce6d2b7f99
alx <asuftin@gmail.com>
2016-03-18 20:27:34 -0400

    initial commit

---
+++ WebApplication1_test/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace WebApplication1_test.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ WebApplication1_test/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication1_test.Startup))]
+namespace WebApplication1_test
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
